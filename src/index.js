cbe8fecfca203fd49add00afe9e4d2c69f5fb141
Alexander Shutilov <avshutilov@yandex.ru>
2020-09-15 12:17:57 +0300

    override ImageTag when passing to Docker

--- src/index.js
+++ src/index.js
@@ -9,6 +9,11 @@ async function action() {
   const { dockerfile, workspace, actionFolder } = Action;
 
   const buildParameters = await BuildParameters.create();
+  const baseImage = {
+    version: buildParameters.version,
+    platform: buildParameters.platform,
+    toString: () => `gableroux/unity3d@sha256:02c9488c23edcd3453a731ac9ef0ce53226daeab80870a54f5163192742b8e7c`,
+  }
   if (buildParameters.kubeConfig) {
     core.info('Building with Kubernetes');
     await Kubernetes.runBuildJob(buildParameters, new ImageTag(buildParameters));
@@ -18,7 +23,7 @@ async function action() {
     const builtImage = await Docker.build({
       path: actionFolder,
       dockerfile,
-      baseImage: `gableroux/unity3d@sha256:02c9488c23edcd3453a731ac9ef0ce53226daeab80870a54f5163192742b8e7c`,
+      baseImage,
       uid: buildParameters.uid,
       gid: buildParameters.gid,
     });
